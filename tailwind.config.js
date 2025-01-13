2d6cad05fe526075d4c2f77775c571e5f10f715c
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2025-01-13 20:49:32 +0530

    Add commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       fontFamily: {
 
         'protest': ['Protest Revolution', 'cursive'],
-        'noto': ['Noto Sans', 'cursive'],
+        'noto': ['Noto-Sans', 'cursive'],
         'imonoid': ['Imonoid', 'monospace'],
         'protest-guerrilla': ['"Protest Guerrilla"', 'cursive'],
       },
