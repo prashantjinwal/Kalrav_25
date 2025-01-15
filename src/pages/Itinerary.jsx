8c97e3f5d6bdf999901abf1ac33f9e7d54ae75ba
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2025-01-15 21:18:22 +0530

    Add

--- src/pages/Itinerary.jsx
+++ src/pages/Itinerary.jsx
@@ -2,7 +2,7 @@
 export default function Itinerary() {
   return (
     <div className="h-[80vh] flex flex-col  justify-center items-center">
-        <h2 className="font-protest font-semibold text-3xl text-white ">COMING SOON...</h2>
+        <h2 className="font-protest font-semibold text-3xl text-white animate-pulse ">COMING SOON...</h2>
         <div><h2 className="text-black  ">( 近日公開 )</h2></div>
         
     </div>
