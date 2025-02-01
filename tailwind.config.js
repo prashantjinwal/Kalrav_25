6db8d4003671c1e96d954ef095f5968fe861d681
Aakash Kumar Jha <aakash13102005@gmail.com>
2025-02-01 14:33:48 +0530

    loader , and test animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +7,6 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-
         'protest': ['Protest Revolution', 'cursive'],
         'noto': ['Noto-Sans'],
         'imonoid': ['Imonoid', 'monospace'],
@@ -26,14 +25,56 @@ export default {
           from: { transform: 'translateX(-50%)' },
           to: { transform: 'translateX(0%)' }
         },
+        animateToTop: {
+          '100%': { transform: 'translateY(0)' }
+        },
+        shrinkTranslate: {
+          '0%': { transform: 'scale(2) translateY(4rem)' },
+          '100%': { transform: 'scale(1) translateY(0)' }
+        },
+
       },
       animation: {
         'infinite-scroll': 'infinite-scroll 10s linear infinite',
         "scroll-left": 'scrollLeft 20s linear infinite',
         "scroll-right": 'scrollRight 20s linear infinite',
+        "shrinkAndTranslateT": 'shrinkTranslate 2s ease-in-out',
+        "animateToTop": 'animateToTop 1s ease-in-out forwards',
+      },
+      animationDelay: {
+        '0s': '0s',
+        '1s': '1s',
+        '2s': '2s',
+        '3s': '3s',
+        '4s': '4s',
+        '5s': '5s',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.animate-delay-0s': {
+          'animation-delay': '0s',
+        },
+        '.animate-delay-1s': {
+          'animation-delay': '1s',
+        },
+        '.animate-delay-2s': {
+          'animation-delay': '2s',
+        },
+        '.animate-delay-3s': {
+          'animation-delay': '3s',
+        },
+        '.animate-delay-4s': {
+          'animation-delay': '4s',
+        },
+        '.animate-delay-5s': {
+          'animation-delay': '5s',
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    },
+  ],
 }
 
