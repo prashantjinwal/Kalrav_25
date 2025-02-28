f9c447ebe974cde7815e974e60273c21eccf73ce
Aakash Kumar Jha <aakash13102005@gmail.com>
2025-03-01 00:57:04 +0530

    about and theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,13 +19,13 @@ export default function Home() {
       img_op: 80
     },
 
-    // {
-    //   title: "ABOUT",
-    //   content:
-    //     "Kalrav Reincarnated\n\nWith the return of the legendary Kalrav, the atmosphere crackles with excitement, like a katana striking steel! This year’s festival embodies a spirit of togetherness and artistic excellence, much like the harmonious union of Yin and Yang. With over 20,000 students from 100+ colleges across the state eagerly awaiting its revival, Kalrav is set to be grander than ever.\n\nThis year, Kalrav blossoms like a vibrant Japanese matsuri. From foot-tapping global hits to soul-stirring vocals, the musical lineup promises to be larger than ever, echoing the energy of past headliners like Honey Singh, Jubin Nautiyal, Guru Randhawa, and Akhil Sachdeva.\n\nAs the lanterns glow softly against the night sky, Kalrav 2025 becomes more than just an event—it transforms into an experience, a realm where traditions meet innovation, and strangers become lifelong friends. As artists, musicians, and performers take the stage, the campus will bloom with talent, much like a field of sakura welcoming the arrival of spring.\n\nThis is not just a celebration; it is a passage into a new world. So step into this dimension of wonder, where Kalrav is not just a festival, but a journey into the extraordinary!",
-    //   content_bg_image: "/images/samurai/3.png",
-    //   img_op: 40
-    // }
+    {
+      title: "ABOUT",
+      content:
+        "Kalrav Reincarnated\n\nWith the return of the legendary Kalrav, the atmosphere crackles with excitement, like a katana striking steel! This year’s festival embodies a spirit of togetherness and artistic excellence, much like the harmonious union of Yin and Yang. With over 20,000 students from 100+ colleges across the state eagerly awaiting its revival, Kalrav is set to be grander than ever.\n\nThis year, Kalrav blossoms like a vibrant Japanese matsuri. From foot-tapping global hits to soul-stirring vocals, the musical lineup promises to be larger than ever, echoing the energy of past headliners like Honey Singh, Jubin Nautiyal, Guru Randhawa, and Akhil Sachdeva.\n\nAs the lanterns glow softly against the night sky, Kalrav 2025 becomes more than just an event—it transforms into an experience, a realm where traditions meet innovation, and strangers become lifelong friends. As artists, musicians, and performers take the stage, the campus will bloom with talent, much like a field of sakura welcoming the arrival of spring.\n\nThis is not just a celebration; it is a passage into a new world. So step into this dimension of wonder, where Kalrav is not just a festival, but a journey into the extraordinary!",
+      content_bg_image: "/images/samurai/3.png",
+      img_op: 40
+    }
 
 
   ];
@@ -92,60 +92,68 @@ export default function Home() {
 
       {/* <div className="w-full lg:w-1/2 mx-auto py-6"> */}
       {/* about us section */}
-      {/* <div className="about">
-          <div className="flex items-center  gap-4 ml-4">
-            <div className="w-2 h-2 bg-[#FF00A6] rounded-full -mt-1">
-            </div>
-            <h2 className="font-bebas-neue text-2xl text-white"
-              style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)", }}>
-              ABOUT US
-            </h2>
-          </div>
-          <div className="about-content py-5 overflow-hidden w-full">
-            <div className="">
-              <img src="images/home/aboutUs.png" alt="" className=" h-full object-cover" />
-            </div>
-          </div>
-          <div className="py-5 relative z-10">
-            <div className="absolute -left-[3rem]  -z-10  bottom-[60%]    "><img src="/images/home/cartoon1.png" className="h-[19rem] w-auto" alt="" /></div>
-            <div className="z-20">
-              <img src="/images/home/aboutUs2.png" alt="" className=" w-full h-full object-cover" />
-            </div>
-          </div>
-        </div> */}
-      {/* theme section */}
-      {/* <div className="theme">
-          <div className="flex items-center  gap-4 ml-4">
-            <div className="w-2 h-2 bg-[#00FFDD] rounded-full -mt-1">
-            </div>
-            <h2 className="font-bebas-neue text-2xl text-white"
-              style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)", }}>
-              THEME
-            </h2>
-          </div>
-          <div className="py-5 relative z-10 overflow-hidden">
-            <div className="absolute -right-[3rem]  -z-10  bottom-[55%]    "><img src="/images/home/cartoon2.png" className="h-[19rem] w-auto" alt="" /></div>
-            <div className="z-20">
-              <img src="/images/home/theme.png" alt="" className="w-full h-full object-cover" />
-            </div>
-          </div>
-        </div>
-      </div> */}
 
 
       <div className="w-full sm:w-9/12 md:w-[65%] lg:w-1/2 mx-auto">
-        {/* Cards Section */}
-        <div className="p-8 sm:mt-10 flex flex-col gap-6 mx-auto ">
-          {cardContents.map((card, index) => (
-            <div key={index}>
-              <Card
-                title={card.title}
-                content={card.content}
-                content_bg_image={card.content_bg_image}
-                img_op={card.img_op}
-              />
+
+
+        <div className="about-theme-container px-4 py-5 ">
+          {/* about section */}
+          <div className="about">
+            <div className="flex items-center gap-3">
+              <div className="w-2 h-2 mb-2 bg-[#FF00A6] rounded-full"></div>
+              <h2 className=" text-[1.8rem] sm:text-3xl md:text-4xl font-bebas-neue text-white" style={{ textShadow: '0px 4px 4px 0px rgba(0, 0, 0, 1)' }}>ABOUT US</h2>
             </div>
-          ))}
+            <div style={{ boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)', textShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.3)' }} className="content  text-white my-2 bg-white/10 rounded-2xl lg:rounded-3xl px-3 lg:px-5 py-2 lg:py-4 font-bebas-neue font-light flex flex-col gap-4">
+              <p className="">Kalrav Reincarnated</p>
+              <p className="">With the return of the legendary Kalrav, the atmosphere crackles with excitement, like a katana striking steel! This year’s festival embodies a spirit of togetherness and artistic excellence, much like the harmonious union of Yin and Yang. With over 20,000 students from 100+ colleges across the state eagerly awaiting its revival, Kalrav is set to be grander than ever.</p>
+              <p className="">
+                This year, Kalrav blossoms like a vibrant <span className=" text-[#EEFF00]"> Japanese matsuri. </span>From foot-tapping global hits to soul-stirring vocals, the musical lineup promises to be larger than ever, echoing the energy of past headliners like  <span className="text-[#eeff00]">
+                  Honey Singh, Jubin Nautiyal, Guru Randhawa, and Akhil Sachdeva.
+                </span>
+              </p>
+              <p className="">
+                As the lanterns glow softly against the night sky, Kalrav 2025 becomes more than just an event—it transforms into an experience, a realm where traditions meet innovation, and strangers become lifelong friends. As artists, musicians and performers take the stage,the campus will bloom with talent,much like a field of sakura welcoming the arrival of spring.
+              </p>
+              <p className="">
+                <span className="text-[#eeff00]">
+                  This is not just a celebration; it is a passage into a new world
+                </span>
+                .So step into this dimension of wonder, where Kalrav is not just a festival, but a journey into the extraordinary!
+              </p>
+
+            </div>
+            <div style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }} className="py-7  text-sm font-druk-wide-bold flex items-center flex-col gap-4">
+              <h3 className="text-[#EEFF00]">20,000+ STUDENTS</h3>
+              <h3 className="text-white">FROM</h3>
+              <h3 className="text-[#FF00A6]">100+ COLLEGES</h3>
+            </div>
+          </div>
+
+
+
+          {/* theme section */}
+
+          <div className="about">
+            <div className="flex items-center gap-3">
+              <div className="w-2 h-2 mb-2 bg-[#FF00A6] rounded-full"></div>
+              <h2 className=" text-[1.8rem] sm:text-3xl md:text-4xl font-bebas-neue text-white" style={{ textShadow: '0px 4px 4px 0px rgba(0, 0, 0, 1)' }}>Theme</h2>
+            </div>
+            <div style={{ boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)', textShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.3)' }} className="content  text-white my-2 bg-white/10 rounded-2xl lg:rounded-3xl px-3 lg:px-5 py-2 lg:py-4 font-bebas-neue font-light flex flex-col gap-4">
+              <p className="text-[#eeff00]">Theme: '' Yūrei no Yoru - Spirits, Samurai, and Sakura ''</p>
+              <p className="">Get ready to enter the mystical world of ancient Japan, where honor, folklore, and spirits collide under the moonlit sky! Yūrei no Yoru —“Night of the Spirits”—reveals a world in which samurai warriors serve by the Bushidō code, and cherry blossoms whisper secrets from ruined myths.</p>
+              <p className="">
+                Engage your mind with the forces of the Tengu, where the strength of the ronin will be tested in trials, and the breathtaking sights of the Bon Odori as they dance in perfect harmony, will captivate you. Let your soul be won by everything from the devious methods of Oda Nobunaga to the magical stories of Kitsune. The land of the rising sun casts a spell.
+              </p>
+              <p className="">
+                This KALRAV, the torii gates will stand open, the lanterns will glow and history will echo through the past—Celebrate with us as we embrace the spirit of Japan! But remember, mortals, “EVEN THE GODS BOW TO THE SAMURAI’S HONOR!”
+              </p>
+
+            </div>
+
+          </div>
+
+
         </div>
 
         <div className="px-12 py-2 flex justify-center">
@@ -194,6 +202,7 @@ export default function Home() {
         </div>
       </div>
     </div>
+
 
   );
 }
