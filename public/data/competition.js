7b2d0672504553d45511c237dcd44f134fd74a07
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2025-03-02 14:36:57 +0530

    Add comp

--- public/data/competition.js
+++ public/data/competition.js
@@ -1,7 +1,7 @@
 const VSC  = [
     {
         title:"VSC",
-        src:"/public/images/comp/vsc1.jpeg",
+        src:"images/comp/vsc1.jpeg",
         link:"https://www.instagram.com/vsc.dduc?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
     },
     
@@ -13,17 +13,17 @@ const VSC  = [
 const Polaroid = [
     {
         title:"Polaroid",
-        src:"/public/images/comp/polroid (1).png",
+        src:"images/comp/polroid (1).png",
         link:"https://forms.gle/UAJqc1Zu2TD8EgUN6"
     },
     {
         title:"Polaroid",
-        src:"/public/images/comp/polroid (2).png",
+        src:"images/comp/polroid (2).png",
         link:"https://forms.gle/9pLXzmnmjik89mUY7"
     },
     {
         title:"Polaroid",
-        src:"/public/images/comp/polroid (3).png",
+        src:"images/comp/polroid (3).png",
         link:"https://forms.gle/see3djrkTrDXRaw67"
     }
 ]
@@ -31,22 +31,22 @@ const Polaroid = [
 // const Rhapsody =[
 //     {
 //         title:"Rhapsody",
-//         src:"/public/images/comp/raaga1 (1).jpeg",
+//         src:"images/comp/raaga1 (1).jpeg",
 //         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
 //     },
 //     {
 //         title:"Rhapsody",
-//         src:"/public/images/comp/raaga1 (2).jpeg",
+//         src:"images/comp/raaga1 (2).jpeg",
 //         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
 //     },
 //     {
 //         title:"Rhapsody",
-//         src:"/public/images/comp/raaga1 (3).jpeg",
+//         src:"images/comp/raaga1 (3).jpeg",
 //         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
 //     },
 //     {
 //         title:"Rhapsody",
-//         src:"/public/images/comp/raaga1 (4).jpeg",
+//         src:"/images/comp/raaga1 (4).jpeg",
 //         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
 //     },
     
@@ -56,22 +56,22 @@ const Polaroid = [
 const Kalamkaar  =[
     {
         title:"Kalamkaar ",
-        src:"/public/images/comp/kalamkaar (1).jpeg",
+        src:"images/comp/kalamkaar (1).jpeg",
         link:"https://forms.gle/QyMNrPqsviy2nE7P9"
     },
     {
         title:"Kalamkaar ",
-        src:"/public/images/comp/kalamkaar (2).jpeg",
+        src:"images/comp/kalamkaar (2).jpeg",
         link:"https://forms.gle/WMQh6o4yzjVM3Tn18"
     },
     {
         title:"Kalamkaar ",
-        src:"/public/images/comp/kalamkaar (3).jpeg",
+        src:"images/comp/kalamkaar (3).jpeg",
         link:"#"
     },
     {
         title:"Kalamkaar ",
-        src:"/public/images/comp/kalamkaar (4).jpeg",
+        src:"images/comp/kalamkaar (4).jpeg",
         link:"#"
     },
     
@@ -81,22 +81,22 @@ const Kalamkaar  =[
 const Raaga  = [
     {
         title:"Raaga",
-        src:"/public/images/comp/raaga1 (1).jpeg",
+        src:"images/comp/raaga1 (1).jpeg",
         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
     },
     {
         title:"Raaga",
-        src:"/public/images/comp/raaga1 (2).jpeg",
+        src:"images/comp/raaga1 (2).jpeg",
         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
     },
     {
         title:"Raaga",
-        src:"/public/images/comp/raaga1 (3).jpeg",
+        src:"images/comp/raaga1 (3).jpeg",
         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
     },
     {
         title:"Raaga",
-        src:"/public/images/comp/raaga1 (4).jpeg",
+        src:"images/comp/raaga1 (4).jpeg",
         link:"https://docs.google.com/forms/d/1xl868_IuT6w3tZvAi7O5bmk-bGh0qIPWlHgw-6lXzUo/edit?ts=67bef609"
     },
       
@@ -123,7 +123,7 @@ const Raaga  = [
 const Enactus  = [
     {
         title:"Enactus",
-        src:"/public/images/comp/enc.jpeg",
+        src:"images/comp/enc.jpeg",
         link:"https://www.instagram.com/enactusdduc"
     },
     
@@ -133,7 +133,7 @@ const Enactus  = [
 const FinS   = [
     {
         title:"Fin-S ",
-        src:"/public/images/comp/fins.jpeg",
+        src:"images/comp/fins.jpeg",
         link:"https://bit.ly/3XkTwPC "
     },
     
@@ -143,12 +143,12 @@ const FinS   = [
 const wdc  = [
     {
         title:"Womens Development Cell",
-        src:"/public/images/comp/wdc (1).jpeg",
+        src:"images/comp/wdc (1).jpeg",
         link:"#"
     },
     {
         title:"Womens Development Cell",
-        src:"/public/images/comp/wdc (1).jpeg",
+        src:"images/comp/wdc (1).jpeg",
         link:"#"
     },
     
@@ -160,12 +160,12 @@ const wdc  = [
 const nE  = [
     {
         title:"North-east cell",
-        src:"/public/images/comp/NE (1).jpeg",
+        src:"images/comp/NE (1).jpeg",
         link:"https://forms.gle/x3vKgaNTTFDtyG2f6"
     },
     {
         title:"North-east cell",
-        src:"/public/images/comp/NE (2).jpeg",
+        src:"images/comp/NE (2).jpeg",
         link:"https://forms.gle/x3vKgaNTTFDtyG2f6"
     },
     
@@ -177,7 +177,7 @@ const nE  = [
 const EOC  = [
     {
         title:"EOC",
-        src:"/public/images/comp/EOC.jpeg",
+        src:"images/comp/EOC.jpeg",
         link: "#"
     },
      
@@ -185,7 +185,7 @@ const EOC  = [
 const SPIC_MACAY  = [
     {
         title:"SPIC MACAY",
-        src:"/public/images/comp/sp.jpeg",
+        src:"images/comp/sp.jpeg",
         link: "#"
     },
      
@@ -196,12 +196,12 @@ const SPIC_MACAY  = [
 const Yuva  = [
     {
         title:"Yuva",
-        src:"/public/images/comp/yava (1).jpeg",
+        src:"images/comp/yava (1).jpeg",
         link: "https://forms.gle/HXjo1TLQQ4xZojqi9"
     },
     {
         title:"Yuva",
-        src:"/public/images/comp/yava (2).jpeg",
+        src:"images/comp/yava (2).jpeg",
         link:"https://forms.gle/5UqBDWL7aEX6hX4T6"
     },
      
@@ -209,17 +209,17 @@ const Yuva  = [
 const Yavanika  = [
     {
         title:"Yavanika",
-        src:"/public/images/comp/yavinika (1).jpeg",
+        src:"images/comp/yavinika (1).jpeg",
         link: "https://forms.gle/HQZqkvyUMRXep5ea6"
     },
     {
         title:"Yavanika",
-        src:"/public/images/comp/yavinika (2).jpeg",
+        src:"images/comp/yavinika (2).jpeg",
         link: "https://forms.gle/jK3RWMDTSrQZ4iwcA"
     },
     {
         title:"Yavanika",
-        src:"/public/images/comp/yavinika (3).jpeg",
+        src:"images/comp/yavinika (3).jpeg",
         link: "https://forms.gle/c2YNMjd4L3H1NcaR7"
     },
     
@@ -229,12 +229,12 @@ const Yavanika  = [
 const Ambedkar = [
     {
         title:"Ambedkar Study Circle",
-        src:"/public/images/comp/ASC (1).jpeg",
+        src:"images/comp/ASC (1).jpeg",
         link: "https://forms.gle/wRLpRinQ1d5rnQVo6 "
     },
     {
         title:"Ambedkar Study Circle",
-        src:"/public/images/comp/ASC (2).jpeg",
+        src:"images/comp/ASC (2).jpeg",
         link: "https://forms.gle/d4x6iALwhW3WDND8A"
     },
 ]
@@ -242,7 +242,7 @@ const Ambedkar = [
 const Escapade = [
     {
         title:"Escapade",
-        src:"/public/images/comp/esp (1).jpeg",
+        src:"images/comp/esp (1).jpeg",
         link: "#"
     },
     
@@ -250,12 +250,12 @@ const Escapade = [
 const NSS = [
     {
         title:"NSS",
-        src:"/public/images/comp/NSS (1).jpeg",
+        src:"images/comp/NSS (1).jpeg",
         link: "https://docs.google.com/forms/d/e/1FAIpQLSdjtmQ9HJ5peyi-4fHYVy2MPxiOSfYfzyYoe4rXsJ0X06rTnQ/viewform"
     },
     {
         title:"NSS",
-        src:"/public/images/comp/NSS (2).jpeg",
+        src:"images/comp/NSS (2).jpeg",
         link: "https://docs.google.com/forms/d/e/1FAIpQLSdnyhZfM2CyFI76URRQSbKEJhCCawBuBlyVC8ZFCLFqwLSzLg/viewform"
     },
     
